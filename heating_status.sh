ceafdde0160973c82026a0c309e8856d7c67b992
Martin Vondráček <mvondr@gmail.com>
2018-05-05 14:54:06 +0200

    Message if season is over.

--- heating_status.sh
+++ heating_status.sh
@@ -6,6 +6,8 @@
 HOME_PATH=$POLTERGEIST_HOME
 
 DATA_FILE_NAME=$HOME_PATH/heating.old
+SEASON_FILE_NAME=$HOME_PATH/season_on.dat
+SEASON_END_FILE_NAME=$HOME_PATH/season_end.dat
 
 if [ ! -f $DATA_FILE_NAME ]; then
    # File does not exist, exiting silently
@@ -25,18 +27,24 @@ fi
 DATA=$(cut -f2 -d: $DATA_FILE_NAME)
 
 TEXT="Should be fulfilled"
-TEXT_DATE=$(date -d @$DATA_TIMESTAMP +'Status: topení je od %Y-%m-%d %H:%M')
 ACTUAL_DATE=$(date +%s)
-DAYS_COUNT=$(( ($ACTUAL_DATE - $DATA_TIMESTAMP) / 86400 ))
-if [ $DAYS_COUNT -gt 0  ]
-then
-    LENGTH="$DAYS_COUNT dní"
+
+if [ -f $SEASON_FILE_NAME ] ; then
+    TEXT_DATE=$(date -d @$DATA_TIMESTAMP +'Status: heating is from %Y-%m-%d %H:%M')
+    DAYS_COUNT=$(( ($ACTUAL_DATE - $DATA_TIMESTAMP) / 86400 ))
+    if [ $DAYS_COUNT -gt 0  ]
+    then
+         LENGTH="$DAYS_COUNT days"
+    else
+        LENGTH=$(date -d"0 $ACTUAL_DATE seconds - $DATA_TIMESTAMP seconds" +'%H:%M hours')
+    fi
+    LENGTH="(for $LENGTH)"
+    (echo $DATA | grep -Eq  ^0.*) && TEXT="$TEXT_DATE off $LENGTH"
+    (echo $DATA | grep -Eq  ^1.*) && TEXT="$TEXT_DATE ON LENGTH"
 else
-    LENGTH=$(date -d"0 $ACTUAL_DATE seconds - $DATA_TIMESTAMP seconds" +'%H:%M hodin')
+    SEASON_END=$(cat $SEASON_END_FILE_NAME)
+    TEXT=$(date -d @$SEASON_END +'Poltergeist: season finished on %Y-%m-%d.')
 fi
-LENGTH="($LENGTH)"
-(echo $DATA | grep -Eq  ^0.*) && TEXT="$TEXT_DATE vypnuto $LENGTH"
-(echo $DATA | grep -Eq  ^1.*) && TEXT="$TEXT_DATE ZAPNUTO $LENGTH"
 
 # send email
 $HOME_PATH/print_info.sh | mail -s"$TEXT" mvondr+rpi.heating@gmail.com
