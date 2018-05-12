26cf395f2ade2ad07f293490011301203496a3d0
Martin Vondráček <mvondr@gmail.com>
2018-05-12 08:47:32 +0200

    Difference after heating season is over.

--- heating_graph24.sh
+++ heating_graph24.sh
@@ -1,13 +1,19 @@
-# create graph of yesterday heating, outside temperature and humifity
+# create graph of yesterday heating / weather, outside temperature and humidity
 # and send it by email
 
 HOME_PATH=$POLTERGEIST_HOME
+GRAPH_FILE=$HOME_PATH/graphs/heating24.png
+SEASON_FILE=$HOME_PATH/season_on.dat
 
 DATE_END="today 00:00"
 PERIOD=1d
-GRAPH_FILE=$HOME_PATH/graphs/heating24.png
-GRAPH_TITLE="Topení za posledních 24h"
 
+if [ -f $SEASON_FILE ] ; then
+  GRAPH_TITLE_PREFIX=Weather
+else
+  GRAPH_TITLE_PREFIX=Heating
+fi
+
+GRAPH_TITLE="$GRAPH_TITLE_PREFIX for last 24 hours"
 $HOME_PATH/draw_graph.sh "$DATE_END" "$PERIOD" "$GRAPH_FILE" "$GRAPH_TITLE"
-
 echo "" | mail -s"$GRAPH_TITLE" -A$GRAPH_FILE mvondr+rpi.heating@gmail.com
